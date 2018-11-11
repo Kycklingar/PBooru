43fa199538e2c98b8a36a90578f38f0a4c0c02ac
kycklingar <apanalex@hotmail.com>
2018-11-11 17:50:39 +0100

    set unique constraint on alias_from on the alias table

--- out/sql/up1.sql
+++ out/sql/up1.sql
@@ -40,9 +40,8 @@ CREATE TABLE IF NOT EXISTS tags(
 );
 
 CREATE TABLE IF NOT EXISTS alias(
-    alias_from INT NOT NULL REFERENCES tags(id),
-    alias_to INT NOT NULL REFERENCES tags(id),
-    CONSTRAINT alias_to_from UNIQUE (alias_from, alias_to)
+    alias_from INT PRIMARY KEY NOT NULL REFERENCES tags(id),
+    alias_to INT NOT NULL REFERENCES tags(id)
 );
 
 CREATE TABLE IF NOT EXISTS comics(
