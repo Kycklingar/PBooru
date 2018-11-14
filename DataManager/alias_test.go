919471f190f2ed2c7ad1ea22871b02e06e02032e
kycklingar <apanalex@hotmail.com>
2018-11-14 15:01:33 +0100

    remove unusable tests

--- DataManager/alias_test.go
+++ DataManager/alias_test.go
@@ -1,91 +1,24 @@
 package DataManager
 
 import (
-	"database/sql"
-	"log"
 	"testing"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
-var testDB *sql.DB
-
-func openTestDB(t *testing.T) *sql.DB {
-	if testDB != nil {
-		return testDB
-	}
-	var err error
-	testDB, err = sql.Open("sqlite3", "../testDB.db")
-	if err != nil {
-		t.Error("Failed to open testDB.db")
-	}
-	err = update(testDB, "../sql")
-	if err != nil {
-		t.Error("Failed to update testDB.db")
-		return nil
-	}
-	return testDB
-}
-
 func TestAliasSave(t *testing.T) {
-	log.SetFlags(log.Llongfile)
 	db := openTestDB(t)
-
-	al := NewAlias()
-
-	al.Tag().Parse("new")
-	al.To(db).Parse("al")
-
-	err := al.Save(db)
-	if err != nil {
-		t.Error("Saving alias failed: ", err)
+	if db == nil {
+		t.Error("Failed to open testdb")
 	}
+	defer db.Close()
 
-	al = NewAlias()
+	a := NewAlias()
+	a.Tag.Tag = "tag"
+	a.Tag.Namespace.Namespace = "namespace"
 
-	al.Tag().Parse("new")
+	a.To.Tag = "tag2"
+	a.To.Namespace.Namespace = "none"
 
-	if al.Tag().ID(db) == 0 {
-		t.Error("Save alias failed: tag doesn't exist")
-	}
-
-	if al.To(db).ID(db) == 0 {
-		t.Error("Save alias failed: to doesn't exist")
-	}
-
-	al = NewAlias()
-	al.Tag().Parse("al")
-
-	if al.From(db) == nil {
-		t.Error("Save alias failed: from is empty")
-	}
-
-}
-
-func TestAliasFrom(t *testing.T) {
-	db := openTestDB(t)
-
-	al := NewAlias()
-	al.Tag().Parse("al")
-
-	if al.From(db) == nil {
-		t.Error("Alias from is nil, expecting 1 result")
-	}
-
-	for _, tag := range al.From(db) {
-		if tag.ID(db) == 0 {
-			t.Error("Alias from tag is empty")
-		}
-	}
-}
-
-func TestAliasTo(t *testing.T) {
-	db := openTestDB(t)
-
-	al := NewAlias()
-	al.Tag().Parse("new")
-
-	if al.To(db).ID(db) == 0 {
-		t.Error("Alias to is empty")
+	if err := a.Save(db); err != nil {
+		t.Error("Failed to save alias", err)
 	}
 }
