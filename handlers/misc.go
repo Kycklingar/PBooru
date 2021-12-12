bf7f6501ef923d2c95f27e30ff734235e595a702
Kycklingar <tvekling@protonmail.com>
2021-12-12 15:12:30 +0100

    no log on render

--- handlers/misc.go
+++ handlers/misc.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"math"
 	"net/http"
 	"strconv"
@@ -30,7 +29,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p interface{}) {
 	err := Templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		//http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
+		//log.Println(err)
 	}
 }
 
