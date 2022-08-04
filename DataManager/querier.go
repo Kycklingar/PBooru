44f7f7a0fbde5dcdc50e431a74d37f4d8b194b1c
Kycklingar <tvekling@protonmail.com>
2022-08-05 00:08:40 +0200

    create row querying interface

--- DataManager/querier.go
+++ DataManager/querier.go
@@ -5,6 +5,15 @@ import (
 	"log"
 )
 
+type (
+	scanner    func(...any) error
+	rowScanner func(scanner) error
+	rows       func(rowScanner) error
+	rowsQuery  interface {
+		Query(string, ...any) (*sql.Rows, error)
+	}
+)
+
 func commitOrDie(tx *sql.Tx, err *error) {
 	var terr error
 	if *err != nil {
@@ -15,5 +24,24 @@ func commitOrDie(tx *sql.Tx, err *error) {
 
 	if terr != nil {
 		log.Println(err)
+	}
+}
+
+func query(q rowsQuery, query string, values ...any) rows {
+	return func(scan rowScanner) error {
+		rows, err := q.Query(query, values...)
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err = scan(rows.Scan)
+			if err != nil {
+				return err
+			}
+		}
+
+		return rows.Err()
 	}
 }
